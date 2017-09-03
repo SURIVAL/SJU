e1b685f69843b1f771526c3f4808302eaec4c117
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 17:42:41 +0530

    armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,19 @@
+#sju
+#include<stdio.h>
+void main()
+{
+int n,i=1,t;
+printf("enter the n value");
+scanf("%d",&n);
+n1=n;
+while(n>0)
+{
+t=n%10;
+i=i+*+*+*+;
+n1=n/10;
+}
+if(n1==i)
+printf("%d is armstrong",n1);
+else
+printf("%d is not a armstrong",n1);
+}
