21dca2765ef69181f967d975ee6b03466cf2a8bd
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 21:11:26 +0530

    nature.c

---
+++ nature.c
@@ -0,0 +1,14 @@
+#sju
+#include<stdio.h>
+void main()
+{
+int n,i,sum=0;
+printf("enter the postive integer:"):
+scanf("%d",&n);
+for(i=1;i<=n;++i)
+{
+sum+=i;
+}
+printf("sum=%d",sum);
+return 0;
+}
